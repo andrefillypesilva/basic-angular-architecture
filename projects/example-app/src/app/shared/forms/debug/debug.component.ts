f4814afb918888ad530eef011f2bc62f5334e7f6
André Pereira da Silva <andre.fillype@vortal.biz>
2020-07-21 18:35:45 +0100

    Implementation of form-debug component

--- projects/example-app/src/app/shared/forms/debug/debug.component.ts
+++ projects/example-app/src/app/shared/forms/debug/debug.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'custom-debug',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./debug.component.scss']
 })
 export class DebugComponent implements OnInit {
+
+  @Input() form: NgForm;
 
   constructor() { }
 
