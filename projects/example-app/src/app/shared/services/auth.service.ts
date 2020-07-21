0e02a5619c7af263ca9fe4970fd613b8fa81cdb3
André Pereira da Silva <andre.fillype@vortal.biz>
2020-07-21 18:56:36 +0100

    Implementation of Auth Service

--- projects/example-app/src/app/shared/services/auth.service.ts
+++ projects/example-app/src/app/shared/services/auth.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+// Environment
+import { environment } from '../../../environments/environment';
+
+// Interfaces
+import { IHttpResponseObject } from '../../models/interfaces/i-http-response-object';
+
+// Models
+import { User } from '../../models/classes/user';
+
+// Services
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private urlServer: string;
+
+  constructor(
+    private _router: Router,
+    private _http: HttpClient,
+    private _localStorageService: LocalStorageService
+  ) {
+    this.urlServer = `${environment.api}/`;
+  }
+
+  login(_user: User): Observable<IHttpResponseObject<User>> {
+    return this._http.post<IHttpResponseObject<User>>(`${this.urlServer}authenticate`, _user);
+  }
+
+  getUser(_user: User): Observable<User> {
+    return this._http.get<IHttpResponseObject<User>>(this.urlServer)
+    .pipe(
+      map((response: IHttpResponseObject<User>) => response.object)
+    );
+  }
+
+  logout(_redirectRoute: string = '/'): void {
+    this._localStorageService.removeUserLocal();
+    this._router.navigate([_redirectRoute]);
+  }
+
+  save(_user: User): Observable<IHttpResponseObject<User>> {
+    this._localStorageService.saveUserLocal(_user);
+    return this.create(_user);
+  }
+
+  private create(_user: User): Observable<IHttpResponseObject<User>> {
+    return this._http.post<IHttpResponseObject<User>>(this.urlServer, _user)
+      .pipe(
+        take(1)
+      );
+  }
 }
