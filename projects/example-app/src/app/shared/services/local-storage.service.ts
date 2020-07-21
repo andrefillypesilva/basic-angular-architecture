f9f75bb209ff0ec77d72a4f6c001440ab2685c14
André Pereira da Silva <andre.fillype@vortal.biz>
2020-07-21 18:31:00 +0100

    Creation of Shared Service

--- projects/example-app/src/app/shared/services/local-storage.service.ts
+++ projects/example-app/src/app/shared/services/local-storage.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+// Models
+import { User } from '../../models/classes/user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
 
   constructor() { }
+
+  getUserLocal(): User {
+    return JSON.parse(JSON.stringify(localStorage.getItem('user')));
+  }
+
+  getUserTokenLocal(): string {
+    if(localStorage.getItem('token')) {
+      return JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    }
+    return '';
+  }
+
+  saveUserLocal(user: User): void {
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', JSON.stringify(user.token));
+  }
+
+  removeUserLocal(): void {
+    localStorage.setItem('user', '');
+    localStorage.setItem('token', '');
+  }
 }
